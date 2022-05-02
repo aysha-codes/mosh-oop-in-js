bf821bb78244d23458cf9b715c175127a9965e32
Aysha S <aysha.springston@gmail.com>
2022-05-02 13:02:18 -0500

    Constructor function

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ function createCircle(radius) {
     }
 }
 const circle = createCircle(1);
+
+// Constructor Function
+function Circle(radius) {
+    this.radius - radius;
+    this.draw - function() {
+        console.log('draw');
+    }
+}
+
+const another = new Circle(1);
