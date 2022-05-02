70a893deb41f5abd3a8cefe1449ba9a4c51f97ca
Aysha S <aysha.springston@gmail.com>
2022-05-02 12:42:35 -0500

    Object literals

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function() {
+        console.log('draw');
+    }
+};
+
+circle.draw();
