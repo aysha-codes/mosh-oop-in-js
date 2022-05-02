0a8001982ed11900e83d255d6f7ebdccc26c9d4c
Aysha S <aysha.springston@gmail.com>
2022-05-02 14:09:42 -0500

    Data Types and References

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-// Value Types
-// Number, String, Boolean, Symbol (new in ES6), undefined, null
-// Reference Types
-// Object, Function, Array
-
-let x = { value: 10 };
-let y = x;
-
-x.value = 20;
-//When we use a reference type (like object), the value is tied to the object, not the variable
-// it's stored somewhere else in memory, so when we copy it the address gets copied rather than the value
-// Primitives are copied by their value
-// Objects are copied by their reference
+let number = 10;
+function increase(number){
+    number++; // completely independent of the above declared variable
+              // AFter the function is done, it goes out of scope
+}
+increase(number);
+console.log(number);
