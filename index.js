57321411056a2a421bf91366858abd67ece490fb
Aysha S <aysha.springston@gmail.com>
2022-05-02 13:34:28 -0500

    Functions are Objects

--- index.js
+++ index.js
@@ -6,13 +6,8 @@ function Circle(radius) {
     }
 }
 
-const Circle1 = new Function('radius', `
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
-`);
-
-const circle = new Circle1(1);
+Circle.call({}, 1) // Identical to expression line 11 call
+// passing the below without new sets the target of this to the window in JS
+Circle.apply({}, [1, 2, 3]); // arguements are passed as an array
 
 const another = new Circle(1);
