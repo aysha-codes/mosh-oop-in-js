bd1f79dfc0e730af4046da34091e281fcb5a6877
Aysha S <aysha.springston@gmail.com>
2022-05-03 13:25:51 -0500

    Adding/Removing Properties

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ circle.location = { x: 1 };
 const propertyName = 'location';
 circle[propertyName] = { x: 1 };
 // IF you have special characters or spaces, use the above bracket notation
+delete['location'] 
+// OR
+delete circle.location
+// Delete data from an object you don't want the client to see, like passwords
