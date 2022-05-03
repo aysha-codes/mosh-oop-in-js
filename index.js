8fa4601bd5d1713b79156d85bff20c307553144f
Aysha S <aysha.springston@gmail.com>
2022-05-03 14:42:09 -0500

    Enumerating Properties

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ function Circle(radius) {
 
 const circle = new Circle(10);
 
-// Can add something extra as needed to objects
-circle.location = { x: 1 };
-const propertyName = 'location';
-circle[propertyName] = { x: 1 };
-// IF you have special characters or spaces, use the above bracket notation
-delete['location'] 
-// OR
-delete circle.location
-// Delete data from an object you don't want the client to see, like passwords
+// We can use a for in loop to emumerate over properties
+for (let key in circle) {
+    if (typeof circle[key] !== 'function') // prevents return of functions, only values
+        console.log(key, circle[key]) // logs properties line by line
+}
+
+const keys = Object.keys(Circle); // dot notation to log each key in an array but can't filter
+console.log(keys);
+
+if ('radius' in circle) // checks for existence
+    console.log('Circle has a radius.');
