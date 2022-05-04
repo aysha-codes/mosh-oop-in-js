9d5d93fc3fc6a6a688380dc6eec4d777304a0f03
Aysha S <aysha.springston@gmail.com>
2022-05-04 13:03:15 -0500

    Exercise

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-function Circle(radius) {
-    this.radius = radius;
+// Stopwatch Object Exercise
+// Stopwatch()
+// duration, reset, start, stop
+// Duration is at 0
+// start begins count; calling again throws an error (Stopwatch has already started)
+// stop pauses count; calling it again throws an error (Stopwatch is not started)
+// reset puts the duration back to 0
+// duration is in seconds up to 3rd decimal place
 
-    let defaultLocation = { x: 0, y: 0 };
+function Stopwatch(){
+    this.duration = 0
+    let started = 0
+    this.start = function(){
+        // Begin count in seconds
+            if (started === 1)
+                    throw new Error('Stopwatch has already started');
+                
+                started = 1
+        startTime = new Date();
 
-    this.getDefaultLocation = functio() {
-        return defaultLocation;
     };
-
-    this.draw = function() {
-        computeOptimumLocation(0.1);
-
-        console.log('draw');
-    }
-
-    Object.defineProperty(this, 'defaultLocation', { // USed to define getters and setters
-        get: function() { // read only property
-            return defaultLocation;
-        },
-        set: function(value) { // allow setting of the property from the outside
-            if (!value.x || !value.y) // throw an error if invalid
-                throw new Error('Invalid locaiton');
-
-            defaultLocation = value;
-        }
-    });
+    this.stop = function(){
+        // Stop count
+            if (started === 0)
+                    throw new Error('Stopwatch is not started');
+                
+                started = 0
+        
+        endTime = new Date();
+        var timeDiff = endTime - startTime; //in ms
+        // strip the ms
+        timeDiff /= 1000;
+      
+        // get seconds 
+        var seconds = Number(timeDiff.toFixed(3));
+        this.duration += seconds;
+    };
+    this.reset = function(){
+        this.duration = 0;
+    };
 }
-
-const circle = new Circle(10);
-circle.defaultLocation = 1; // Throws the error
-circle.draw();
