8313b907fb80e3f8455411c1120b1a6a857fe80c
Aysha S <aysha.springston@gmail.com>
2022-05-04 13:18:07 -0500

    Mosh's exercise solution

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
 // Stopwatch Object Exercise
-// Stopwatch()
-// duration, reset, start, stop
-// Duration is at 0
-// start begins count; calling again throws an error (Stopwatch has already started)
-// stop pauses count; calling it again throws an error (Stopwatch is not started)
-// reset puts the duration back to 0
-// duration is in seconds up to 3rd decimal place
+// Mosh's Solution
+function Stopwatch() {
+    let startTime, endTime, running, duration = 0;
 
-function Stopwatch(){
-    this.duration = 0
-    let started = 0
-    this.start = function(){
-        // Begin count in seconds
-            if (started === 1)
-                    throw new Error('Stopwatch has already started');
-                
-                started = 1
+    this.start = function() {
+        if (running)
+            throw new Error('Stopwatch has already started.');
+
+        running = true;
+
         startTime = new Date();
+    };
 
-    };
-    this.stop = function(){
-        // Stop count
-            if (started === 0)
-                    throw new Error('Stopwatch is not started');
-                
-                started = 0
-        
+    this.stop = function() {
+        if (!running)
+            throw new Error('Stopwatch is not started.');
+
+        running = false;
         endTime = new Date();
-        var timeDiff = endTime - startTime; //in ms
-        // strip the ms
-        timeDiff /= 1000;
-      
-        // get seconds 
-        var seconds = Number(timeDiff.toFixed(3));
-        this.duration += seconds;
+
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds;
     };
-    this.reset = function(){
-        this.duration = 0;
+
+    this.reset = function() {
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
     };
+
+    Object.defineProperty(this, 'duration', {
+        get: function() { return: duration; }
+    });
 }
